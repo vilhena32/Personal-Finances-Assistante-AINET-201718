57c90c04d423ccdcfa55cb4597c1d6cbcd5d3883
vilhena32 <joana.vilhena.32@gmail.com>
2018-05-08 13:28:59 +0100

    Index , top , bottom e nav-bar

---
+++ projeto/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	@include('partials.index.top')
+</head>
+<body>
+	@include('partials.index.nav')
+	
+	@include('partials.statistics.data')
+
+	@include('partials.index.bottom')
+</body>
+</html>
+
+
