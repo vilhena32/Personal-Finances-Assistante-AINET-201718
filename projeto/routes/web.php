5c5e42cc92a7d5d48ada3b94ce5fbcd4c7822ae8
Ricardo <ricardo.casal@hotmail.com>
2018-05-11 13:23:26 +0100

    Rotas
    Rotas(falta alterar os controllers e os nomes)

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -17,4 +17,66 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'UserController@index')->name('home');
+Route::get('/', 'UserController@index')->name('home');
+
+Route::get('/register', 'UserController@index')->name('users.index');
+Route::post('/register', 'UserController@blocked')->name('users.blocked');
+
+Route::post('/login', 'UserController@create')->name('users.create');
+
+Route::get('/users', 'UserController@store')->name('users.store');
+
+Route::get('/users', 'UserController@search')->name('users.search');
+
+Route::patch('users/{user}/block', 'UserController@search_block')->name('users.search_block');
+Route::patch('users/{user}/unblock', 'UserController@search_block')->name('users.search_block');
+Route::patch('users/{user}/promote', 'UserController@search_block')->name('users.search_block');
+Route::patch('users/{user}/demote', 'UserController@search_block')->name('users.search_block');
+
+Route::patch('/me/password', 'UserController@edit')->name('users.edit');
+
+Route::put('/me/profile', 'UserController@update')->name('users.update');
+
+Route::get('/profiles', 'UserController@destroy')->name('users.destroy');
+
+Route::get('/me/associates', 'UserController@destroy')->name('users.destroy');
+
+Route::get('/associate-off', 'UserController@destroy')->name('users.destroy');
+
+Route::get('/accounts/{users}', 'UserController@destroy')->name('users.destroy');
+Route::get('/accounts/{users}/opened', 'UserController@destroy')->name('users.destroy');
+Route::get('/accounts/{users}/closed', 'UserController@destroy')->name('users.destroy');
+
+Route::delete('/account/{account}', 'UserController@destroy')->name('users.destroy');
+Route::patch('/account/{account}/close', 'UserController@destroy')->name('users.destroy');
+
+Route::patch('/accounts/{account}/reopen', 'UserController@destroy')->name('users.destroy');
+
+Route::post('/account', 'UserController@destroy')->name('users.destroy');
+
+Route::put('/account/{account}', 'UserController@destroy')->name('users.destroy');
+
+Route::get('/movements/{account}', 'UserController@destroy')->name('users.destroy');
+
+Route::get('/movements/{account}/create', 'UserController@destroy')->name('users.destroy');
+Route::post('/movements/{account}/create', 'UserController@destroy')->name('users.destroy');
+Route::get('/movement/{movement}', 'UserController@destroy')->name('users.destroy');
+Route::put('/movement/{movement}', 'UserController@destroy')->name('users.destroy');
+Route::delete('/movement/{movement}', 'UserController@destroy')->name('users.destroy');
+
+Route::post('/documents/{movement}', 'UserController@destroy')->name('users.destroy');
+
+Route::delete('/document/{document}', 'UserController@destroy')->name('users.destroy');
+Route::get('/document/{document}', 'UserController@destroy')->name('users.destroy');
+
+Route::get('/dashboard/{user}', 'UserController@destroy')->name('users.destroy');
+
+Route::get('/me/associates', 'UserController@destroy')->name('users.destroy');
+
+Route::delete('/me/associates/{user}', 'UserController@destroy')->name('users.destroy');
+//Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+
+
+//Route::get('/home', 'HomeController@index');
