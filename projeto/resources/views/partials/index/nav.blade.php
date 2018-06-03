909d3025d3f6ef8049ee187ec3678bcc43c75a61
joanavilhena <2131107@my.ipleiria.pt>
2018-06-03 21:13:37 +0100

    Index está diferente. Dropdown tem bug. Ao clicar no link e redirecionar para a pagina ele ja n pode ser outra vez acedido

--- projeto/resources/views/partials/index/nav.blade.php
+++ projeto/resources/views/partials/index/nav.blade.php
@@ -1,24 +1,29 @@
 <div class="d-flex flex-column flex-md-row align-items-csenter p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-      <h5 class="my-0 mr-md-auto font-weight-normal" >Company name</h5>
-      <nav class="my-2 my-md-0 mr-md-3">
-        <a class="p-2 text-dark" href="{{route('home')}}">Home Page</a>
-        @if(Auth::user())
-        <a class="p-2 text-dark" href="{{route('accounts',Auth::user())}}">Accounts</a>
-        @endif
-        <a class="p-2 text-dark" href="#">Support</a>
-        <a class="p-2 text-dark" href="#">Pricing</a>
-      </nav>
+  <h5 class="my-0 mr-md-auto font-weight-normal" ><a class="p-2 text-dark" href="{{route('home')}}">Personal Finances App</a></h5>
+  <nav class="my-2 my-md-0 mr-md-3">
 
-      @if(!Auth::user())
-        <a class="btn btn-outline-primary" href="{{route('register')}}">Sign up</a>
-        <a class="btn btn-outline-primary" href="{{route('login')}}">Login</a>
-      @else
-        <a class="btn btn-outline-primary" href="{{route('showProfile')}}">MyProfile</a>
-        <a class="btn btn-outline-primary" href="{{route('logout')}}">Logout</a>
-      @endif
+  </nav>
+
+
+  @if(!Auth::user())
+  <a class="btn btn-outline-primary" href="{{route('register')}}">Sign up</a>
+  <a class="btn btn-outline-primary" href="{{route('login')}}">Login</a>
+  @else
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      {{Auth::user()->name}}
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="#">My Movements</a> 
+      <a class="dropdown-item" href="{{route('accounts',Auth::user())}}">My Accounts</a>
+      <a class="dropdown-item" href="{{route('showProfile')}}">MyProfile
+      <a class="dropdown-item" href="{{route('logout')}}">Logout</a>
+      </div>
     </div>
+    @endif
+  </div>
+</div>
 
 
 
 
- 
