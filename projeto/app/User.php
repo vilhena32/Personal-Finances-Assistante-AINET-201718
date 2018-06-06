500dee4f0b53c54f3f5a5377540f33ffeb87e581
Airtik <nunomcq@hotmail.com>
2018-06-06 12:50:30 +0100

    Auto stash before merge of "master" and "origin/master"

--- projeto/app/User.php
+++ projeto/app/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
         'name', 'email', 'password','phone', 'profile_photo'
     ];
 
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -28,45 +29,42 @@ class User extends Authenticatable
     ];
 
 
-
     public function getType()
     {   
         return $this->admin ? 'Administrator' : 'Regular';
     }
+
 
     public function getStatus()
     {   
         return $this->blocked ? 'Blocked' : 'Unblocked';
   
     }
+
+
     public function getPhoto(){
         if ($this->profile_photo==NULL) {
             return asset('storage/profiles/default.png');
-        }
-        else{
-         return asset('storage/profiles/'.$this->profile_photo);
+        } else {
+            return asset('storage/profiles/'.$this->profile_photo);
         }
     }
-
 
 
     public function associates()
     {
-        
         return $this->belongsToMany('App\User','associate_members','main_user_id','associated_user_id')->withPivot('created_at');
     }
+
 
     public function accounts()
     {
         return $this->hasMany('App\Account','owner_id');
     }
 
+
     public function associatedOf()
     {
-        
         //return $this->belongsToMany('App\User','associate_members','main_user_id','this->id')->withPivot('created_at');
     }
-
-    
-
 }
